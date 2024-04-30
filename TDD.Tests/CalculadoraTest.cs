5d21e2f76a29d32bd4095e46d3948beae2e69ce2
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-04-30 17:37:37 -0300

    test: Created tests for Historico

--- TDD.Tests/CalculadoraTest.cs
+++ TDD.Tests/CalculadoraTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using TDD;
+using System.Collections.Generic;
 
 namespace TDD.Tests
 {
@@ -67,6 +68,64 @@ namespace TDD.Tests
             void act() => calculadora.Dividir(1, 0);
 
             Assert.Throws<DivideByZeroException>(act);
+        }
+
+        [Fact]
+        public void Historico_AposUmaOperacao_RetornaListaComUmItem()
+        {
+            var calculadora = new Calculadora();
+
+            calculadora.Somar(1, 1);
+            List<string> historico = calculadora.Historico();
+            int resultado = historico.Count;
+
+            Assert.Equal(1, resultado);
+        }
+
+        [Fact]
+        public void Historico_AposQuatroOperacoes_RetornaListaComTresItens()
+        {
+            var calculadora = new Calculadora();
+
+            calculadora.Somar(1, 1);
+            calculadora.Somar(1, 1);
+            calculadora.Somar(1, 1);
+            calculadora.Somar(1, 1);
+            List<string> historico = calculadora.Historico();
+            int resultado = historico.Count;
+
+            Assert.Equal(3, resultado);
+        }
+
+        [Fact]
+        public void Historico_AposOperacao_RetornaOperacaoCorreta()
+        {
+            var calculadora = new Calculadora();
+
+            calculadora.Somar(1, 2);
+            List<string> historico = calculadora.Historico();
+            string resultado = historico[0];
+
+            Assert.Equal("1 + 2 = 3", resultado);
+        }
+
+        [Fact]
+        public void Historico_AposQuatroOperacoes_RetornaOperacoesCorretas()
+        {
+            var calculadora = new Calculadora();
+
+            calculadora.Somar(1, 2);
+            calculadora.Somar(3, 4);
+            calculadora.Somar(5, 6);
+            calculadora.Somar(7, 8);
+            List<string> historico = calculadora.Historico();
+            string resultado1 = historico[0];
+            string resultado2 = historico[1];
+            string resultado3 = historico[2];
+
+            Assert.Equal("3 + 4 = 7", resultado1);
+            Assert.Equal("5 + 6 = 11", resultado2);
+            Assert.Equal("7 + 8 = 15", resultado3);
         }
     }
 }
