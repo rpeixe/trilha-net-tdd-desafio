b82a0209d1c27be816817d3cd07233c92b4adaa6
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-04-30 11:55:42 -0300

    feat: Created placeholder methods

--- TDD/Calculadora.cs
+++ TDD/Calculadora.cs
@@ -6,6 +6,29 @@ namespace TDD
 {
     public class Calculadora
     {
+        public int Somar(int num1, int num2)
+        {
+            return 0;
+        }
 
+        public int Subtrair(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int Dividir(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public List<string> Historico()
+        {
+            return null;
+        }
     }
 }
