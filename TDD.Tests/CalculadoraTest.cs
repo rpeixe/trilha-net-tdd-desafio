06c34abd757a1f8f05eaa9446f367b09d1400c03
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-04-30 16:58:30 -0300

    test: Created tests for Somar

--- TDD.Tests/CalculadoraTest.cs
+++ TDD.Tests/CalculadoraTest.cs
@@ -6,10 +6,17 @@ namespace TDD.Tests
 {
     public class CalculadoraTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(1, -2, -1)]
+        [InlineData(1, 0, 1)]
+        public void Somar_DoisNumeros_RetornaSoma(int numero1, int numero2, int resultadoEsperado)
         {
-            
+            var calculadora = new Calculadora();
+
+            int resultado = calculadora.Somar(numero1, numero2);
+
+            Assert.Equal(resultadoEsperado, resultado);
         }
     }
 }
